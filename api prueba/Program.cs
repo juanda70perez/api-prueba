5d207109283a11c9e6e2b169d77ea90a0b40166d
Juan david perez sotelo <juan-10david@hotmail.com>
2023-05-17 13:54:54 -0500

    Agregar archivos de proyecto.

---
+++ api prueba/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
+string link = "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20";
+
+using (var httpClient = new HttpClient())
+{
+    var response = await httpClient.GetAsync(link);
+    
+    if (response.IsSuccessStatusCode)
+    {
+        if (response.Content != null)
+        {
+            var datos = response.Content.ReadAsStringAsync();
+            var json = JObject.Parse(datos.Result);
+            //Console.WriteLine(json["results"][0]["name"]);
+            foreach (var dato in json["results"])
+            {
+                Console.WriteLine(dato["name"]);
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("no funciono");
+    }
+
+}
